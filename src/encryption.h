c57fc536aa0696c9dd0d78b620cd8f8e5f34dd4a
ryzen7800xt <beccaleb.13@gmail.com>
2026-05-04 21:29:23 +1000

    Add uem256_ctx structure and related functions

--- src/encryption.h
+++ src/encryption.h
@@ -8,6 +8,17 @@ extern "C"
 {
 #endif
 
+    typedef struct
+    {
+        uint64_t state[4];
+        size_t length;
+        unsigned char buffer[8];
+        size_t buffer_len;
+    } uem256_ctx;
+
+    void uem256_init(uem256_ctx *ctx);
+    void uem256_update(uem256_ctx *ctx, const unsigned char *input, size_t length);
+    void uem256_final(uem256_ctx *ctx, unsigned char output[32]);
     void uem256_hash(const unsigned char *input, size_t length, unsigned char output[32]);
     void uem256_hex(const unsigned char digest[32], char hex[65]);
 
