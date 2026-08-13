0fe92604cbf7614665e63d3982a1d80334af518f
ryzen7800xt <beccaleb.13@gmail.com>
2026-08-14 09:21:55 +1000

    Update __main__.py

--- uem256/__main__.py
+++ uem256/__main__.py
@@ -1,6 +1,6 @@
-from ._core import hash
+from ._core import hash # import the hash function
 
-if __name__ == "__main__":
+if __name__ == "__main__": # declare 
     import sys
     if len(sys.argv) > 1:
         print(hash(sys.argv[1]))
